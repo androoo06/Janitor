19057d2816e4fee66878671f930101bb453e3553
Andrew Miller <84351389+androoo06@users.noreply.github.com>
2021-12-16 18:46:30 -0500

    Update Janitor.lua
    Removed all error-throwing and instead provided necessary warns, and a return value of `true` to mark that the method (would have) raised an error. Also adjusted some functionality of the clean and add methods

--- Janitor.lua
+++ Janitor.lua
@@ -1,39 +1,47 @@
+-- My personal janitor class, by androooo06
+
 local Janitor = {}
 Janitor.__index = Janitor
 
-function Janitor.new()
+function Janitor.new(...)
 	local self = setmetatable({},Janitor)
 	self.libraries = {}
 	self.libraries.Default = {}
+	for _,lib in pairs({...})do
+		self:CreateLibrary(tostring(lib))
+	end
 	return self
 end
 
 function Janitor:CreateLibrary(name)
-	assert(self.libraries[name]==nil,"Library already exists.")
+	if (self.libraries[name]) then
+		warn("Passed Library already exists."); return true
+	end
 	self.libraries[name] = {}
 end
 
 function Janitor:RemoveLibrary(name)
-	assert(self.libraries[name]~=nil,"Passed Library is nil.")
+	if not (self.libraries[name]) then
+		warn("Passed Library is nil."); return true
+	end
 	self:Clean(name)
 	self.libraries[name] = {}
 end
 
 function Janitor:Add(object,library)
-	assert(object~=nil,"Argument 1 is missing or nil.")
-	assert(library~=nil,"Argument 2 is missing or nil.")
-	assert(self.libraries[library]~=nil,"Passed Library is nil.")
-	
-	if (typeof(object)=="table")then
+	if (object == nil) then
+		warn("Argument 1 is missing or nil."); return true		
+	end
+	local lib = library or "Default"
+	if (typeof(object) == "table")then
 		for _,item in pairs(object) do
-			table.insert(self.libraries[library],item)
+			table.insert(self.libraries[lib],item)
 		end
 	else
-		table.insert(self.libraries[library],object)
+		table.insert(self.libraries[lib],object)
 	end
 end
 
--- @alias CleanAll
 function Janitor:Clean(library)
 	local function clean(tab)
 		for _,item in pairs(tab) do
@@ -45,27 +53,32 @@ function Janitor:Clean(library)
 			item = nil
 		end
 	end
+	
 	if (self.libraries[library]) then
 		clean(self.libraries[library])
 		self.libraries[library] = {}
-	else
+	elseif (library == nil) then
 		for name,lib in pairs(self.libraries) do
 			clean(lib)
 			self.libraries[name] = nil
+		end
+	else
+		warn("No library ["..tostring(library).."] was found to clean.")
+		return true
+	end
+end
+
+function Janitor:CleanAll()
+	for libName,_ in pairs(self.libraries)do
+		if (self.libraries[libName]) then
+			self:Clean(libName)
 		end
 	end
 end
 
 function Janitor:Destroy()
-	self:CleanAll()
-	self = nil
-end
-
--- Aliases / Wrappers --
-
--- @function Clean
-function Janitor:CleanAll()
 	self:Clean()
+	self = nil
 end
 
 return Janitor
