c0fb974c2fc1908b26ef0ee68b5f0dea5c79d4a9
Andrew Miller <84351389+androoo06@users.noreply.github.com>
2022-06-07 01:23:22 -0400

    Update Janitor.lua
    refactored a bit; moved clean method to be called statically as opposed to forcing instance-creation.

--- Janitor.lua
+++ Janitor.lua
@@ -1,22 +1,41 @@
--- My personal janitor class, by androooo06
-
 local Janitor = {}
 Janitor.__index = Janitor
 
+function Janitor.__Clean(tab)
+	for k, item in pairs (tab) do
+		if (typeof(item) == "Instance") and not (item:IsA("Player")) then
+			item:Destroy()
+		elseif (typeof(item) == "RBXScriptConnection") then
+			item:Disconnect()
+		elseif (type(item) == "table") and (item.Destroy) then
+			item:Destroy()
+		elseif (type(item) == "table") then
+			Janitor.__Clean(item) -- deep search
+		end
+
+		tab[k] = nil
+		item   = nil
+	end
+end
+
 function Janitor.new(...)
-	local self = setmetatable({},Janitor)
-	self.libraries = {}
+	local self = setmetatable({}, Janitor)
+
+	self.libraries 		   = {}
 	self.libraries.Default = {}
-	for _,lib in pairs({...})do
+
+	for _,lib in pairs {...} do
 		self:CreateLibrary(tostring(lib))
 	end
+
 	return self
 end
 
 function Janitor:CreateLibrary(name)
 	if (self.libraries[name]) then
-		warn("Passed Library already exists."); return true
+		warn("Passed Library ["..name.."] already exists."); return true
 	end
+
 	self.libraries[name] = {}
 end
 
@@ -24,15 +43,18 @@ function Janitor:RemoveLibrary(name)
 	if not (self.libraries[name]) then
 		warn("Passed Library is nil."); return true
 	end
+
 	self:Clean(name)
 	self.libraries[name] = {}
 end
 
-function Janitor:Add(object,library)
+function Janitor:Add(object, library)
 	if (object == nil) then
 		warn("Argument 1 is missing or nil."); return true		
 	end
+
 	local lib = library or "Default"
+
 	if (typeof(object) == "table")then
 		for _,item in pairs(object) do
 			table.insert(self.libraries[lib],item)
@@ -43,23 +65,12 @@ function Janitor:Add(object,library)
 end
 
 function Janitor:Clean(library)
-	local function clean(tab)
-		for _,item in pairs(tab) do
-			if (typeof(item) == "Instance") then
-				item:Destroy()
-			elseif (typeof(item) == "RBXScriptConnection") then
-				item:Disconnect()
-			end
-			item = nil
-		end
-	end
-	
 	if (self.libraries[library]) then
-		clean(self.libraries[library])
+		Janitor.__Clean(self.libraries[library])
 		self.libraries[library] = {}
 	elseif (library == nil) then
 		for name,lib in pairs(self.libraries) do
-			clean(lib)
+			Janitor.__Clean(lib)
 			self.libraries[name] = nil
 		end
 	else
@@ -69,10 +80,8 @@ function Janitor:Clean(library)
 end
 
 function Janitor:CleanAll()
-	for libName,_ in pairs(self.libraries)do
-		if (self.libraries[libName]) then
-			self:Clean(libName)
-		end
+	for libName, _ in pairs (self.libraries) do
+		self:Clean(libName)
 	end
 end
 
